6a04de0d77da0554af501330b87ff99c7ec031ba
subham <subham.bhattacharjee1@gmail.com>
2017-09-02 14:55:05 +0530

    Removed Hard coded URLs

--- music/urls.py
+++ music/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
 
 from . import views
-
+app_name = 'music' #for namespacing url, which prevent conflicts between two url of different app with same name
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<album_id>[0-9]+)/$',views.details, name='')
+    url(r'^(?P<album_id>[0-9]+)/$',views.details, name='detail')
 ]
